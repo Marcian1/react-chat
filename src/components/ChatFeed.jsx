837230659d95fe8dea9fc21a48b56dc6a9a4c9c6
Z0060883 <marcian.bondoc-popescu.external@trw.com>
2021-04-20 21:35:53 +0300

    added chat-feed

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const ChatFeed = (props) => {
+    console.log(props);
+    console.log('asdasdsad');
+    return (
+        <div>
+            Chat Feedaaaaaaaaaa
+        </div>
+    );
+}
+
+export default ChatFeed;
